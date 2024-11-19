a5b24788187ab00c43ddbd23aca890f912fb400a
kidiga <juniorprogrammer09@gmail.com>
2024-11-19 16:11:13 +0300

    firebase firestore and storage dependencies setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -77,6 +78,7 @@ dependencies {
     //firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage)
 
 
 }
