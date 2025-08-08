9b911c9722dd58f85c2a5d6f5d0ae77c02cf240f
briank <98683954+kdbrian@users.noreply.github.com>
2025-08-08 07:37:00 +0300

    Initialize koin

--- app/src/main/java/com/kdbrian/templated/TemplatedApp.kt
+++ app/src/main/java/com/kdbrian/templated/TemplatedApp.kt
@@ -1,6 +1,10 @@
 package com.kdbrian.templated
 
 import android.app.Application
+import com.kdbrian.templated.di.templateModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class TemplatedApp : Application() {
@@ -9,5 +13,15 @@ class TemplatedApp : Application() {
 
         if (BuildConfig.DEBUG)
             Timber.plant(Timber.DebugTree())
+
+        startKoin {
+
+            androidLogger()
+            androidContext(this@TemplatedApp)
+
+            modules(
+                templateModules,
+            )
+        }
     }
 }
